ab640b5cc716dee6e228390a9b7a96a6d051e328
rakeshm425 <rakeshm@olivecrypto.com>
2020-07-16 13:29:43 +0530

    loginChecking

--- 02Variables/ConditionalLogins.js
+++ 02Variables/ConditionalLogins.js
@@ -2,11 +2,17 @@ var email = true;
 var facebook = false;
 var google = true;
 
-if(email){
-    console.log("Login success");
-        }
-else if(facebook){
-        console.log("you are not allowed");
+// if(email){
+//     console.log("Login success");
+//         }
+// else if(facebook){
+//         console.log("you are not allowed");
+// }else{
+//         console.log("Login success");
+//     }
+
+if(email || facebook || google){
+        console.log("login success");
 }else{
-        console.log("Login success");
-    }
+        console.log("login unsuccessful");
+}

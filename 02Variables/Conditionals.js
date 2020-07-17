6af78a8b53a446a5e742020660e5f5cf6b9cab1b
rakeshm425 <rakeshm@olivecrypto.com>
2020-07-17 10:20:35 +0530

    ternary operator

--- 02Variables/Conditionals.js
+++ 02Variables/Conditionals.js
@@ -1,7 +1,15 @@
-var temperature = 20;
+// var temperature = 20;
 
-if(temperature > 60){
-    console.log("It too cold outside");
-}else{
-    console.log("Its too hot outside");
-}
+// if(temperature > 60){
+//     console.log("It too cold outside");
+// }else{
+//     console.log("Its too hot outside");
+// }
+
+//Ternary Operator
+
+var a = 5;
+
+var b = (a >= 5) ? "its ok" : "inncorrect entry";
+
+console.log(b);

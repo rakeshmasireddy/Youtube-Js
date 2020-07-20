b6278c410d159ad5719710f3eaa27341c47edfe6
rakeshm425 <rakeshm@olivecrypto.com>
2020-07-20 15:26:16 +0530

    Three Equals to

---
+++ 02Variables/trulyandfaslyvalues.js
@@ -0,0 +1,14 @@
+// undefined
+// null // it doesnot return nothing // due to falsy.
+// ''
+// 0
+// NaN : not an Integer
+
+var user = "2";
+//for very strict checking use ===
+// where == is lossly coupled.
+if (user === 2) {
+    console.log("condition is successfull");
+}
+
+// console.log(2 + "2");

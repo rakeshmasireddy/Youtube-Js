af66c96dabdcca9ceb41b66a9fc548488730979c
rakeshm425 <rakeshm@olivecrypto.com>
2020-07-21 12:25:26 +0530

    functions

---
+++ 03Intermediate/functions.js
@@ -0,0 +1,25 @@
+function sayHello() {
+    console.log("Hello");
+}
+
+sayHello();
+
+function WithVariable(name){
+    console.log("Hello " + name);
+}
+
+WithVariable("Rahul");
+
+function Withvariable(name){
+    console.log(`Hello ${name}, How are you?`);
+}
+
+Withvariable("Rakesh");
+
+function Hi(){
+    return "Welcome";
+}
+
+// Hi(); 
+var greetings = Hi();
+console.log(greetings);
